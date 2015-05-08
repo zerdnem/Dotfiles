57056898f98d93e166d7227b16415794024b7655
zerdnem <zerdnem@gmail.com>
2015-05-08 20:21:06 +0800

    modify emacs a bit

---
+++ emacs.d/config/packages.el
@@ -0,0 +1,6 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
---
+++ emacs.d/config/ui.el
@@ -0,0 +1,6 @@
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(setq visible-bell t)
+(setq initial-scratch-message "")
+(setq inhibit-startup-message t)
