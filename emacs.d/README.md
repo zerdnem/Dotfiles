e439f31b5b39fa9401774145afab577f6a2862a5
kek <kek@Kristys-MacBook-Air.local>
2015-11-24 17:20:59 +0800

    emacs packages

---
+++ emacs.d/README.md
@@ -0,0 +1,8 @@
+##Packages
+* evil
+* evil-surround
+* evil-nerdcommenter
+* emmet
+* base16
+##Screenshot
+![emacs](#)
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,4 +3,4 @@
 
 (load-config "ui.el")
 (load-config "packages.el")
-(load-config "keybind.el")
+(load-theme 'base16-isotope-dark)
