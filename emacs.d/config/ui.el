9a483ff8bd6f88b61afefbba0700d95d313e95eb
zerdnem <zerdnem@gmail.com>
2016-01-28 01:11:05 +0800

    Update ui.el

--- emacs.d/config/ui.el
+++ emacs.d/config/ui.el
@@ -12,3 +12,14 @@
 (set-fringe-mode 0)
 (setq linum-format "%d ")
 (show-paren-mode 1)
+(require 'linum)
+(defun linum-update-window-scale-fix (win)
+  "fix linum for scaled text"
+  (set-window-margins win
+		      (ceiling (* (if (boundp 'text-scale-mode-step)
+				      (expt text-scale-mode-step
+					    text-scale-mode-amount) 1)
+				  (if (car (window-margins))
+				      (car (window-margins)) 1)
+				  ))))
+(advice-add #'linum-update-window :after #'linum-update-window-scale-fix)
